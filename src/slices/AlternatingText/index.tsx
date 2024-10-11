707d7ee072bacce15476d2225e3abb6872e98ef2
오현주 <ohhyunjoo@ohyeonjuui-MacBookPro.local>
2024-10-11 11:36:07 +0900

    FEAT: Alternating Text

--- src/slices/AlternatingText/index.tsx
+++ src/slices/AlternatingText/index.tsx
@@ -6,6 +6,10 @@ import {
   PrismicText,
   SliceComponentProps,
 } from "@prismicio/react";
+import { View } from "@react-three/drei";
+import Scene from "./Scene";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import clsx from "clsx";
 
 /**
  * Props for `AlternatingText`.
@@ -17,22 +21,29 @@ export type AlternatingTextProps =
  * Component for "AlternatingText" Slices.
  */
 const AlternatingText = ({ slice }: AlternatingTextProps): JSX.Element => {
+  const isDesktop = useMediaQuery("(min-width: 768px)", true);
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="alternating-text-container relative text-sky-950"
+      className="alternating-text-container relative bg-yellow-300 text-sky-950"
     >
       <div>
-        <div className="relative grid">
+        <div className="relative z-[100] grid">
           {/* View goes here */}
+          <View className="alternating-text-view absolute left-0 top-0 h-screen w-full">
+            <Scene />
+          </View>
           {slice.primary.text_group.map((item, index) => (
             <div
               key={asText(item.heading)}
               className="alternating-section grid h-screen place-items-center gap-x-12 md:grid-cols-2"
             >
               <div
-                className={index % 2 === 0 ? "col-start-1" : "md:col-start-2"}
+                className={clsx(
+                  index % 2 === 0 ? "col-start-1" : "md:col-start-2",
+                  "max-md: rounded-lg bg-white/20 p-4 backdrop-blur-lg",
+                )}
               >
                 <h2 className="text-balance text-6xl font-bold">
                   <PrismicText field={item.heading} />
